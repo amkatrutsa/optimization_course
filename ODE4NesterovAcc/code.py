f2801d1e87e301340ce48c3a0738ad3e8517bb42
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2017-09-22 12:14:11 +0300

    Fix code

--- ODE4NesterovAcc/code.py
+++ ODE4NesterovAcc/code.py
@@ -26,22 +26,20 @@ def GradientDescent(f, gradf, x0, epsilon, num_iter, line_search,
     res = {"x": x, "num_iter": iteration, "tol": np.linalg.norm(gradf(x))}
     return res
 
-def backtracking(x, descent_dir, **kwargs):
-    f = kwargs["f"]
-    grad_f = kwargs["grad_f"] 
+def backtracking(f, grad_f, x, descent_dir, rule, args=(), **kwargs):
     alpha = 1
-    if kwargs["method"] == "Armijo":
+    if rule == "Armijo":
         beta1 = kwargs["beta1"]
         rho = kwargs["rho"]
-        while f(x + alpha * descent_dir) >= f(x) + beta1 * alpha * grad_f(x).dot(descent_dir) or np.isnan(f(x + alpha * descent_dir)):
+        while f(x + alpha * descent_dir, *args) >= f(x, *args) + beta1 * alpha * grad_f(x, *args).dot(descent_dir) or np.isnan(f(x + alpha * descent_dir, *args)):
             if alpha < 1e-18:
                 break
             alpha *= rho
-    if kwargs["method"] == "Wolfe":
+    if rule == "Wolfe":
         rho = kwargs["rho"]
         beta1 = kwargs["beta1"]
         beta2 = kwargs["beta2"]
-        while (f(x + alpha * descent_dir) >= f(x) + beta1 * alpha * grad_f(x).dot(descent_dir) or np.isnan(f(x + alpha * descent_dir)) and np.abs(descent_dir.dot(grad_f(x + alpha * descent_dir))) > -beta2 * descent_dir.dot(grad_f(x))) or np.isnan(f(x + alpha * descent_dir)):
+        while (f(x + alpha * descent_dir, *args) >= f(x, *args) + beta1 * alpha * grad_f(x, *args).dot(descent_dir) or np.isnan(f(x + alpha * descent_dir, *args)) and np.abs(descent_dir.dot(grad_f(x + alpha * descent_dir, *args))) > -beta2 * descent_dir.dot(grad_f(x, *args))) or np.isnan(f(x + alpha * descent_dir, *args)):
             if alpha < 1e-10:
                 break
             alpha *= rho
